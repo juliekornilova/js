608c91537372c941ea01b5ff196696b6f4fb25d8
Julie Kornilova <y94878194@gmail.com>
2024-04-04 18:40:32 +0300

    Add modul files

--- js_hw4.js
+++ js_hw4.js
@@ -2,5 +2,13 @@
 // function prompt()
 // console.log(year);
 
-console.log(-6 % 3); //-0 -!!!
-console.log(undefined ?? null ?? false);
+// console.log(-6 % 3); //-0 -!!!
+// console.log(undefined ?? null ?? false);
+
+let person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    text = "User cancelled the prompt.";
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+  console.log(text);
