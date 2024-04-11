f264e7cc71e94deedc42129c3e38a45e468de8af
Julie Kornilova <y94878194@gmail.com>
2024-04-11 19:31:10 +0300

    Added hw5 brad.js

---
+++ HW_hillel_js/brad.js
@@ -0,0 +1,25 @@
+//First variant
+
+// var j="*";
+// for (let i=0; i<5; i++)
+// { 
+//     if (i==0){
+//     // console.log(i);
+//     console.log(j);
+//     };
+//     // console.log(i);
+//     console.log(j+="*");
+//     };
+// console.log("merry Cristmas")
+
+// Second variant
+
+let asteriks = "*";
+// for (let i=0; i < 5; i++) {
+    console.log(asteriks);
+    console.log(asteriks + asteriks);
+    console.log(asteriks + asteriks+asteriks);
+    console.log(asteriks + asteriks+asteriks+asteriks);
+    console.log(asteriks + asteriks+asteriks+asteriks+asteriks);
+    // }; // drawing new year tree
+console.log("merry Cristmas")
