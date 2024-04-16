ff90c81669f20383575847090b3ac4b317084d8f
Julie Kornilova <y94878194@gmail.com>
2024-04-16 18:36:23 +0300

    File modified

--- HW_hillel_js/lesson2_hw.js
+++ HW_hillel_js/lesson2_hw.js
@@ -1,4 +1,15 @@
-const num = 4;
-const str = "Kovbasa";
-const result = num + str + num; 
-console.log(result + "\n", typeof result);
+// const num = 4;
+// const str = "Kovbasa";
+// const result = num + str + num; 
+// console.log(result + "\n", typeof result);
+
+// var number = 20;
+// number /= 2;
+// console.log(number);
+
+// var result = true;
+// result += true;
+// console.log(result)
+
+console.log('null as number: ' + (+null) ); // + приводить до числа 0
+console.log('undefined as number: ' + (+undefined) ); //приводить до NaN
