77d56483c34ce9eb7a8b58a807b554c1dda29c50
Julie Kornilova <y94878194@gmail.com>
2024-04-18 08:01:45 +0300

    New version of code in brad.js

--- HW_hillel_js/brad.js
+++ HW_hillel_js/brad.js
@@ -1,25 +1,35 @@
-//First variant
+// First variant with for (let i=0; i < 5; i++) {}
 
-// var j="*";
-// for (let i=0; i<5; i++)
-// { 
-//     if (i==0){
-//     // console.log(i);
-//     console.log(j);
-//     };
-//     // console.log(i);
-//     console.log(j+="*");
-//     };
+let str = '';
+console.log("First variant");
+
+for (let i = 0; i < 5; i++) {
+  str = str + "*";
+  console.log(str);
+}
+
+
+// Second variant do...while
+
+let result = '';
+let i = 0;
+console.log("Second variant");
+
+do {
+  i++;
+  result = result + "*";
+  console.log(result);
+} while (i < 5);
+
+
+
+// Variant with console.log
+
+    // let asteriks = "*";
+    // console.log(asteriks);
+    // console.log(asteriks + asteriks);
+    // console.log(asteriks + asteriks+asteriks);
+    // console.log(asteriks + asteriks+asteriks+asteriks);
+    // console.log(asteriks + asteriks+asteriks+asteriks+asteriks);
+    
 // console.log("merry Cristmas")
-
-// Second variant
-
-let asteriks = "*";
-// for (let i=0; i < 5; i++) {
-    console.log(asteriks);
-    console.log(asteriks + asteriks);
-    console.log(asteriks + asteriks+asteriks);
-    console.log(asteriks + asteriks+asteriks+asteriks);
-    console.log(asteriks + asteriks+asteriks+asteriks+asteriks);
-    // }; // drawing new year tree
-console.log("merry Cristmas")
